37b6785ce87c18f0438a5a95794e0ad6a756bc57
Jauhar Arifin <jauhararifin10@gmail.com>
2023-11-11 23:35:21 +0700

    add crappy favicon

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ mkdir -p build/platform/web
 rm -rf build/platform/web/*
 cp ./platform/web/index.html ./build/platform/web/index.html
 cp ./platform/web/main.js ./build/platform/web/main.js
+cp ./favicon.ico ./build/platform/web/favicon.ico
 cp ./build/nes.wasm ./build/platform/web/nes.wasm
 
 mkdir -p build/platform/web/roms
